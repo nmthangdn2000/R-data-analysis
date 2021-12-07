92aec94ed27a20490c5e004430c44a594f6cd3ec
Uptimum <uptimumdn2000@gmail.com>
2021-12-08 00:36:58 +0700

    .

--- service/home.service.r
+++ service/home.service.r
@@ -8,8 +8,97 @@ homeService <- function(input, output, dataset){
   #   )
   # })
   # Đơn hàng theo ngày
+  # ouputValueBox <- function(id) {
+  #   value <- 0
+  #   name <- " "
+  #   icon <- ""
+  #   if(id == "value_box_1") {
+  #     totalPrice <- 0
+  #     for(i in dataset$Price) {
+  #       if(!is.na(i))
+  #         totalPrice <- totalPrice + i
+  #     }
+  #     name <- "Total Price"
+  #     icon <- icon("money-check-alt")
+  #     value <- totalPrice
+  #   }
+  #   else if(id == "value_box_2") {
+  #     name <- "Products"
+  #     icon <- icon("box-open")
+  #     value <-  length(unique(dataset$Item))
+  #   }
+  #   else if(id == "value_box_3") {
+  #     name <- "Customer"
+  #     icon <- icon("users")
+  #     value <- 1853
+  #   }
+  #   else {
+  #     name <- "Orders"
+  #     icon <- icon("shopping-cart")
+  #     value <- length(unique(dataset$Transaction))
+  #   }
+  #   output[[id]] <- renderValueBox({
+  #     valueBox(
+  #       value, name, icon = icon,
+  #       width = 3
+  #     )
+  #   })
+  # }
+  # 
+  # valueBox <- c("value_box_1", "value_box_2","value_box_3","value_box_4")
+  # for (v in valueBox) ouputValueBox(v)
   
+  outputFilters <- function(id) {
+    value <- "a"
+    label <- "b"
+    list <- c()
+    idUi <- paste("filter_home", tolower(id), sep="_")
+    
+    if(id == "Start_day") {
+      value <- dataset$Date[1]
+      label <- "Start Day"
+    }
+    else {
+      value <- dataset$Date[length(dataset$Date)]
+      label <- "End Day"
+    }
+    
+    print(value)
+    print(label)
+    
+    if(id == "Start_day" || id == "End_day") {
+      id = "Date"
+      list <- c(unique(dataset[[id]]))
+    }
+    
+    output[[idUi]] <- renderUI({
+      selectInput(
+        idUi, 
+        label = label,
+        choices = as.list(list),
+        selected = value,
+        width ="120px"
+      )
+    })
+  }
+  
+  filerTag <- c("Start_day", "End_day")
+  for (i in filerTag) {
+    outputFilters(i)
+  }
+  show <- 15
+  observeEvent(input$input_number_show, {
+    show <- input$input_number_show
+  })
   output$revenue_chart_order <- renderHighchart({
+    filterStartDay <- input$filter_home_start_day
+    filterEndDay <- input$filter_home_end_day
+    show <- input$input_number_show
+    if(!is.null(filterEndDay)) filterEndDay <- as.Date(filterEndDay, "%m/%d/%Y")
+    else filterEndDay <- 2017
+    if(!is.null(filterStartDay)) filterStartDay <- as.Date(filterStartDay, "%m/%d/%Y")
+    else filterStartDay <- 2017
+    
     
     dates <- dataset$Date
     as.Date(dataset$Date, "%m/%d/%Y")
@@ -22,9 +111,12 @@ homeService <- function(input, output, dataset){
       
     }
     
-    data <- data.frame(listDate, totalOrder)
-    colnames(data) <- c("Date", "Order")
-    data <- head(data, 15)
+    data <- data.frame(listDate, totalOrder, listDate)
+    colnames(data) <- c("Date", "Order", "Dtae2")
+    data$Dtae2 <- as.Date(data$Dtae2, "%m/%d/%Y")
+    data <- as_tbl_time(data, index = Dtae2)
+    data <- filter_time(data, filterStartDay ~ filterEndDay)
+    data <- tail(data, as.integer(show))
     
     hchart(data, hcaes(x=Date,y=Order),type="area",color="#43dc80", fillOpacity = 0.3) %>%
       hc_exporting(enabled = TRUE) %>% 
@@ -187,4 +279,5 @@ homeService <- function(input, output, dataset){
     #   plot.background=element_blank()
     # )
   })
+  
 }
